7318abc13a25e135e0ab4e34e534a4301b583a1c
jspark300 <2525js@naver.com>
2017-01-14 17:18:22 +0900

    c1

--- app/src/main/java/com/giant_monkey/reboot/MainActivity.java
+++ app/src/main/java/com/giant_monkey/reboot/MainActivity.java
@@ -9,19 +9,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    PowerManager pm;
+   // PowerManager pm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+      //  pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         Button button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                pm.reboot("null");
+           //     pm.reboot("null");
+
+                try {
+                    Process proc = Runtime.getRuntime()
+                            .exec(new String[]{ "su", "-c", "reboot -p" }); // "reboot"
+                    proc.waitFor();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }

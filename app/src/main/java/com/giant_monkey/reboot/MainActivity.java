a406ff15eeb75c27103bbb0db211b036cd04691e
jaesung park <2525js@naver.com>
2017-05-17 11:24:27 +0900

    리부팅 - 크라이저 소스적용

--- app/src/main/java/com/giant_monkey/reboot/MainActivity.java
+++ app/src/main/java/com/giant_monkey/reboot/MainActivity.java
@@ -1,6 +1,7 @@
 package com.giant_monkey.reboot;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,12 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+       // pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         Button button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                pm.reboot("null");
+              //  pm.reboot("null");
+
+                Intent intent = new Intent("rk.android.rebooting.action");
+                sendBroadcast(intent);
 
 //                try {
 //                    Process proc = Runtime.getRuntime()
